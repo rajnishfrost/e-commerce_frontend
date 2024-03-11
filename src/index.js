60e46097067d2148fa2b48bbd0c17425b2e33f44
rajnishfrost <rajnishfrost@gmail.com>
2024-03-11 16:50:02 +0530

    add a space

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import {Provider} from "react-redux";
 // store.subscribe(()=>console.log(store.getState()));
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  
   <React.StrictMode>
     <Provider store = {store}>
     <App />
